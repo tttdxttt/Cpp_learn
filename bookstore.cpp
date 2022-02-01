48690a7ef7709da26f9e7b288222751b20f1dc1f
heng <tttdxttt@gmail.com>
2022-01-31 20:20:43 -0600

    more use

--- bookstore.cpp
+++ bookstore.cpp
@@ -4,23 +4,46 @@
 using namespace std; 
 int main()
 {
-    Sales_item currItem, Item;
+    Sales_item total;
 
-    if (std::cin >> currItem)
+    if (cin>>total)
     {
-        int cnt = 1;
-        while (std::cin >> Item){
-            if (Item.isbn() == currItem.isbn())
-                ++cnt;
-            else{
-                std::cout << currItem << " occurs "
-                        << cnt << " times" << std::endl;
-                currItem = Item;
-                cnt = 1;
+        Sales_item trans;
+        while (cin>>trans)
+        {
+            if (total.isbn() == trans.isbn())
+                total += trans;
+            else
+            {
+                cout << total << endl;
+                total = trans;
             }
-        } std::cout << currItem << " occurs "
-                << cnt<< " times" << std::endl;
+        }
+        cout << total << endl;
     }
+    else{
+        cerr << "No data"<< endl;
+        return -1; 
+    }
+
+
+    // Sales_item currItem, Item;
+
+    // if (std::cin >> currItem)
+    // {
+    //     int cnt = 1;
+    //     while (std::cin >> Item){
+    //         if (Item.isbn() == currItem.isbn())
+    //             ++cnt;
+    //         else{
+    //             std::cout << currItem.isbn() << " occurs "
+    //                     << cnt << " times" << std::endl;
+    //             currItem = Item;
+    //             cnt = 1;
+    //         }
+    //     } std::cout << currItem << " occurs "
+    //             << cnt<< " times" << std::endl;
+    // }
 
     // first check that item1 and item2 represent the same book
     // if (item1.isbn() == item2.isbn())
